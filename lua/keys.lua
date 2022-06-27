18e646faf885379f1f5a6fe2a2ef886e7f4c7344
gnatseira <gnatseira@gmail.com>
2022-06-28 01:45:50 +0800

    define mappings for auto completion in nvim-cmp.lua

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,11 +1,6 @@
 -- [[ keys.lua ]]
 local map = vim.api.nvim_set_keymap
 
-local opt = {
-  noremap = true,
-  silent = true,
-}
-
 -- remap the key used to leave insert mode
 -- Toggle nvim-tree
 map('n', '<C-n>', [[:NvimTreeToggle]], {})
@@ -14,34 +9,3 @@ map('n', 't', [[:TagbarToggle]], {})
 map('n', 'ff', [[:Telescope find_files]], {})
 map('n', '<C-o>', [[:BufferLineCycleNext<CR>]], {})
 map('n', '<C-p>', [[:BufferLineCyclePrev<CR>]], {})
-
-local pluginKeys = {}
-
--- nvim-cmp 自动补全
-pluginKeys.cmp = function(cmp)
-  return {
-    -- 上一个
-    ['<C-k>'] = cmp.mapping.select_prev_item(),
-    -- 下一个
-    ['<C-j>'] = cmp.mapping.select_next_item(),
-    -- 出现补全
-    ['<A-.>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    -- 取消
-    ['<A-,>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    -- 确认
-    -- Accept currently selected item. If none selected, `select` first item.
-    -- Set `select` to `false` to only confirm explicitly selected items.
-    ['<CR>'] = cmp.mapping.confirm({
-      select = true ,
-      behavior = cmp.ConfirmBehavior.Replace
-    }),
-    -- ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-  }
-end
-
-return pluginKeys
