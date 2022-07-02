ec12918b7516cb4a1c296f52b7c0a6ed52009760
gnatseira <gnatseira@gmail.com>
2022-07-02 18:31:25 +0800

    add a mapping to swith to nvim-tree

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,12 +1,17 @@
 -- [[ keys.lua ]]
 local map = vim.api.nvim_set_keymap
+local opt = {
+    noremap = true,
+    silent = true,
+}
 
 -- remap the key used to leave insert mode
 -- Toggle nvim-tree
-map('n', '<C-n>', [[:NvimTreeToggle]], {})
+map('n', '<C-n>', [[:NvimTreeToggle<CR>]], opt)
 
-map('n', 't', [[:TagbarToggle]], {})
-map('n', 'ff', [[:Telescope find_files]], {})
-map('n', 'fr', [[:Telescope oldfiles]], {})
-map('n', '<C-o>', [[:BufferLineCycleNext<CR>]], {})
-map('n', '<C-p>', [[:BufferLineCyclePrev<CR>]], {})
+map('n', 't', [[:TagbarToggle]], opt)
+map('n', 'ff', [[:Telescope find_files]], opt)
+map('n', 'fr', [[:Telescope oldfiles]], opt)
+map('n', '<C-o>', [[:BufferLineCycleNext<CR>]], opt)
+map('n', '<C-p>', [[:BufferLineCyclePrev<CR>]], opt)
+map('n', '<C-h>', '<C-w>h', opt)
