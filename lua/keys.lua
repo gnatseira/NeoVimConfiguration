c40e0a299a8a6cffca11ab0fe55659fd1ce1c68c
gnatseira <gnatseira@gmail.com>
2022-07-02 19:06:52 +0800

    add mappings of close windows in bufferline.vim

--- lua/keys.lua
+++ lua/keys.lua
@@ -6,12 +6,20 @@ local opt = {
 }
 
 -- remap the key used to leave insert mode
+--
 -- Toggle nvim-tree
 map('n', '<C-n>', [[:NvimTreeToggle<CR>]], opt)
+map('n', '<C-h>', '<C-w>h', opt)
 
-map('n', 't', [[:TagbarToggle]], opt)
-map('n', 'ff', [[:Telescope find_files]], opt)
-map('n', 'fr', [[:Telescope oldfiles]], opt)
+-- tagbar
+map('n', 't', [[:TagbarToggle<CR>]], opt)
+
+-- telescope
+map('n', 'ff', [[:Telescope find_files<CR>]], opt)
+map('n', 'fr', [[:Telescope oldfiles<CR>]], opt)
+
+-- bufferline
 map('n', '<C-o>', [[:BufferLineCycleNext<CR>]], opt)
 map('n', '<C-p>', [[:BufferLineCyclePrev<CR>]], opt)
-map('n', '<C-h>', '<C-w>h', opt)
+map('n', '<C-a>', [[:BufferLinePickClose<CR>]], opt)
+map('n', '<C-s>', [[:BufferLineCloseLeft<CR>:BufferLineCloseRight<CR>]], opt)
