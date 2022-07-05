05ce767a5b38f64f60c138d941a351ff0c52713a
gnatseira <gnatseira@gmail.com>
2022-07-05 23:53:11 +0800

    add a opt to remove warnings in dashboard

--- lua/config/alpha-nvim.lua
+++ lua/config/alpha-nvim.lua
@@ -1,5 +1,9 @@
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
+local opt = {
+    noremap = true,
+    silent = true,
+}
 
 -- Set header
 --[[
@@ -40,12 +44,12 @@ dashboard.section.header.val = {
 -- Set menu
 dashboard.section.buttons.val = {
     --dashboard.button( "Leader f f", "  > Find file", ":Leaderf file --popup<CR>"),
-    dashboard.button( "Leader f f", "  > Find file", ":Telescope find_files<CR>"),
-    dashboard.button( "Leader f r", "  > Recent files", ":Telescope oldfiles<CR>"),
+    dashboard.button( "Leader f f", "  > Find file", ":Telescope find_files<CR>", opt),
+    dashboard.button( "Leader f r", "  > Recent files", ":Telescope oldfiles<CR>", opt),
     --dashboard.button( "Leader f g", "  > Project grep" , ":Leaderf rg --popup<CR>"),
-    dashboard.button( "u", "  > Update plugins" , ":PackerSync<CR>"),
-    dashboard.button( "e", "  > New file" , ":enew <CR>"),
-    dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+    dashboard.button( "u", "  > Update plugins" , ":PackerSync<CR>", opt),
+    dashboard.button( "e", "  > New file" , ":enew <CR>", opt),
+    dashboard.button( "q", "  > Quit NVIM", ":qa<CR>", opt),
 }
 
 --local fortune = require("alpha.fortune")
