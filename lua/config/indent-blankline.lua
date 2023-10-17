29268388583afc24e49079fe0a1488446667d0df
gnatseira <gnatseira@gmail.com>
2023-10-17 17:56:52 +0800

    upgrade to indent-blankline 3

--- lua/config/indent-blankline.lua
+++ lua/config/indent-blankline.lua
@@ -1,30 +1,43 @@
-require("indent_blankline").setup({
-    char = "│",
-    show_first_indent_level = true,
-    filetype_exclude = {
-        "startify",
-        "dashboard",
-        "dotooagenda",
-        "log",
-        "fugitive",
-        "gitcommit",
-        "packer",
-        "vimwiki",
-        "markdown",
-        "json",
-        "txt",
-        "vista",
-        "help",
-        "todoist",
-        "NvimTree",
-        "peekaboo",
-        "git",
-        "TelescopePrompt",
-        "undotree",
-        "flutterToolsOutline",
-        "", -- for all buffers without a file type
+require ("ibl").setup({
+    --char = "│",
+    --show_first_indent_level = true,
+    indent = {
+       char = "│",
     },
-    buftype_exclude = { "terminal", "nofile" },
+    exclude = {
+        filetypes = {
+            "startify",
+            "dashboard",
+            "dotooagenda",
+            "log",
+            "fugitive",
+            "gitcommit",
+            "packer",
+            "vimwiki",
+            "markdown",
+            "json",
+            "txt",
+            "vista",
+            "help",
+            "todoist",
+            "NvimTree",
+            "peekaboo",
+            "git",
+            "TelescopePrompt",
+            "undotree",
+            "flutterToolsOutline",
+            "", -- for all buffers without a file type
+        },
+        buftypes = { 
+            "terminal",
+            "nofile" 
+        },
+    },
+    scope = {
+        enabled = true,
+        show_end = true,
+    },
+    --[[
     show_trailing_blankline_indent = false,
     show_current_context = true,
     context_patterns = {
@@ -44,6 +57,7 @@ require("indent_blankline").setup({
         "import",
     },
     space_char_blankline = " ",
+    ]]--
 })
 -- because lazy load indent-blankline so need readd this autocmd
-vim.cmd("autocmd CursorMoved * IndentBlanklineRefresh")
+--vim.cmd("autocmd CursorMoved * IndentBlanklineRefresh")
